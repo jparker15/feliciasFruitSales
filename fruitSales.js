1ba9a45a3fa8a8a6b2fff31d55f5d304dbf766c8
Jeremy Parker <jeremyeparker@yahoo.com>
2020-04-07 21:24:48 -0400

    add object to fruitSales

--- fruitSales.js
+++ fruitSales.js
@@ -1,6 +1,7 @@
 let inventory = ["Apples","Peaches","Pumpkins","Endives"];
 let cost = [1,1.25,8,5];
 
+let obj = [{fruit:apple},{fruit:peaches},{vegatable:pumpkin}];
 for (let i = 0; i < cost.length; i++) {
     
     console.log(`${inventory[i]}`);
